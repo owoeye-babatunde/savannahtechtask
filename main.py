47dedadda4a4fa38a59e40a59045ff05bff18789
Devopdevs <owoeyebabatunde01@gmail.com>
2023-12-21 09:57:54 -0800

    add a readme file and delete a file

--- main.py
+++ main.py
@@ -2,7 +2,17 @@ import random
 import multiprocessing
 
 def create_dwarf_giant_pairs(employees):
-    unique_employees = {tuple(employee.value()): employee for employee in employees}
+    """
+    create unique dwarf giant pairs from a list of employees while ensuring fairness and 
+    randomness
+    Args:
+        employees: A list of employee objects, each containing three fields.
+
+    Returns:
+        A list of tuples, where each tuple represents a Dwarf-Giant pair (name, name)
+    
+    """
+    unique_employees = {tuple(employee.values()): employee for employee in employees}
     employee_names = list(unique_employees.keys())
 
     random.shuffle(employee_names)
