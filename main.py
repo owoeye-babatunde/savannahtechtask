a245a5568658941641e158e58a7209a707f60348
Devopdevs <owoeyebabatunde01@gmail.com>
2023-12-21 09:09:24 -0800

    write test case

--- main.py
+++ main.py
@@ -9,3 +9,4 @@ def create_dwarf_giant_pairs(employees):
     pairs = [(employee_names[i], employee_names[-i-1]) for i in range(len(employee_names) // 2)]
     return [(unique_employees[pair[0]]["name"], unique_employees[pair[1]]["name"]) for pair in pairs]
 
+
